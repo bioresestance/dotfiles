1f34955b91d8eb9944156f91bf1834ce439f7b0f
Aaron Bromma <aaron@bromma.dev.ca>
2024-12-23 11:43:47 -0800

    add home-manager configuration and integrate with flake.nix

--- flake.nix
+++ flake.nix
@@ -3,23 +3,36 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+    }:
+
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
     {
       nixosConfigurations = {
-
-        Bromma-Laptop = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./configuration.nix
-          ];
+        Bromma-Laptop = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
         };
       };
-
-      # homeConfigurations = {
-
-      # };
+      homeConfigurations = {
+        aaron = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
     };
 }
