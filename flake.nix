39e703b2e038908978ce15911da59b600f7c5df1
Aaron Bromma <aaron@bromma.dev>
2025-11-17 11:32:50 -0800

    Remove unused nix-vscode-extensions input from flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
   outputs =
@@ -15,7 +14,6 @@
       self,
       nixpkgs,
       home-manager,
-      nix-vscode-extensions,
     }@inputs:
 
     let
